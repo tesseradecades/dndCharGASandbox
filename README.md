d14548a4a2604130b75017538b78fdbb9eb69141
tesseradecades <nwe8978@rit.edu>
2019-05-16 19:24:20 -0400

    added initial population generation

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-To run unit tests type `python -m discover -s test`
+To run unit tests type `python -m unittest discover -s test`
--- main/GeneticAlgorithm.py
+++ main/GeneticAlgorithm.py
@@ -1,7 +1,11 @@
+from array import array
 
 #Initial Population
 def generateInitialPopulation(genomeLength:int)->list:
-    return []
+    initialPopulation = []
+    for i in range(4):
+        initialPopulation.append(array('b',[0]*genomeLength))
+    return initialPopulation
 #Fitness Function
 #Selection
 #Crossover
