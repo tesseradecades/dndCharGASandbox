8296f185d963c724b34a3929011720d760dc3529
tesseradecades <nwe8978@rit.edu>
2019-05-17 19:41:35 -0400

    refactored race

--- main/DnD/Character.py
+++ main/DnD/Character.py
@@ -1,30 +1,33 @@
+from AbilityScores import AbilityScores
 from Races import Race
 
 class Character():
 
     #Ability Scores
-    strength_score = 0
-    dexterity_score = 0
-    constitution_score = 0
-    intelligence_score = 0
-    wisdom_score = 0
-    charisma_score = 0
+    abilityScores = {
+        AbilityScores.STRENGTH:0,
+        AbilityScores.DEXTERITY:0,
+        AbilityScores.CONSTITUTION:0,
+        AbilityScores.INTELLIGENCE:0,
+        AbilityScores.WISDOM:0,
+        AbilityScores.CHARISMA:0
+    }
 
     race = None
 
-    def __init__(self, race:Race.Race):
+    def __init__(self, race):
         self.race = race
     def __str__(self)->str:
         ret = f"Race:\t\t{self.race}\
-            \nStrength:\t{self.strength_score}\
-            \nDexterity:\t{self.dexterity_score}\
-            \nConstitution:\t{self.constitution_score}\
-            \n:Intelligence:\t{self.intelligence_score}\
-            \nWisdom:\t\t{self.wisdom_score}\
-            \nCharisma:\t{self.charisma_score}"
+            \nStrength:\t{self.abilityScores[AbilityScores.STRENGTH]}\
+            \nDexterity:\t{self.abilityScores[AbilityScores.DEXTERITY]}\
+            \nConstitution:\t{self.abilityScores[AbilityScores.CONSTITUTION]}\
+            \n:Intelligence:\t{self.abilityScores[AbilityScores.INTELLIGENCE]}\
+            \nWisdom:\t\t{self.abilityScores[AbilityScores.WISDOM]}\
+            \nCharisma:\t{self.abilityScores[AbilityScores.CHARISMA]}"
         return ret
 def main():
-    print(Character(Race.Race.BLACK_DRAGONBORN))
+    print(Character(Race.RACES[0]))
 
 if __name__ == "__main__":
     main()
--- main/DnD/Races/Race.py
+++ main/DnD/Races/Race.py
@@ -23,3 +23,29 @@ class Race(Enum):
     HALF_ELF = 20,
     HALF_ORC = 21,
     TIEFLING = 22
+
+RACES = {
+    0:"Hill Dwarf",
+    1:"Mountain Dwarf",
+    2:"High Elf",
+    3:"Wood Elf",
+    4:"Dark Elf",
+    5:"Lightfoot Halfling",
+    6:"Stout Halfling",
+    7:"Human",
+    8:"Black Dragonborn",
+    9:"Blue Dragonborn",
+    10:"Brass Dragonborn",
+    11:"Bronze Dragonborn",
+    12:"Copper Dragonborn",
+    13:"Gold Dragonborn",
+    14:"Green Dragonborn",
+    15:"Red Dragonborn",
+    16:"Silver Dragonborn",
+    17:"White Dragonborn",
+    18:"Forest Gnome",
+    19:"Rock Gnome",
+    20:"Half-Elf",
+    21:"Half-Orc",
+    22:"Tiefling"
+}
