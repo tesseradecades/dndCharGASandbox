102a44ac69d693a81e71f9c883a152cb591fdf01
tesseradecades <nwe8978@rit.edu>
2019-05-17 18:34:54 -0400

    removed unnecessary code and added algorithm to converge based on change in fitness

--- main/FitnessChange.py
+++ main/FitnessChange.py
@@ -2,12 +2,13 @@ from array import array
 from GeneticAlgorithm import GeneticAlgorithm
 """
 This version of the Genetic Algorithm defines "convergence" as "reaching the
-specified level of fitness"
+specified number of generations without a change in the maximum observed
+fitness"
 """
-class TargetFitness(GeneticAlgorithm):
+class FitnessChange(GeneticAlgorithm):
 
     def __init__(self):
-        super(TargetFitness,self).__init__()
+        super(FitnessChange,self).__init__()
 
     def selection(self,population:list, fitnessFunction)->tuple:
         fitnessDict = {}
@@ -20,16 +21,24 @@ class TargetFitness(GeneticAlgorithm):
         fitnessDict.pop(secondFittest)
         return (population[fittest],population[secondFittest], fittestScore)
 
-    def run(self,genomeLength:int,targetFitness:float,fitnessFunction)->array:
+    def run(self,genomeLength:int,targetGenerations:int,fitnessFunction)->array:
+        generationsWithoutChange = 0
         population = self.generateInitialPopulation(genomeLength)
         parent1, parent2,fittestScore = self.selection(population, fitnessFunction)
         child1,child2 = self.crossoverHelper(parent1,parent2)
         self.mutationHelper(child1)
         self.mutationHelper(child2)
         population = [parent1,parent2,child1,child2]
-        while(fittestScore < targetFitness):
+        while(generationsWithoutChange < targetGenerations):
             print(f"Fitness:\t{fittestScore}")
-            parent1, parent2, fittestScore = self.selection(population, fitnessFunction)
+            parent1, parent2, newFittestScore = self.selection(population, fitnessFunction)
+
+            if(newFittestScore <= fittestScore):
+                generationsWithoutChange+=1
+            else:
+                generationsWithoutChange = 0
+            fittestScore = newFittestScore
+
             child1,child2 = self.crossoverHelper(parent1,parent2)
             self.mutationHelper(child1)
             self.mutationHelper(child2)
@@ -42,8 +51,9 @@ def fitnessFunction(genome: array)-> float:
                     f+=1
             return f
 def main():
-    ga = TargetFitness()
-    best = ga.run(10,10,fitnessFunction)
+    genomeLength = 10
+    ga = FitnessChange()
+    best = ga.run(genomeLength,genomeLength*genomeLength,fitnessFunction)
     print(best)
 if __name__ == '__main__':
     main()
