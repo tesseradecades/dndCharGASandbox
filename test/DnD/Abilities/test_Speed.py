1c0c73d7363b11273c7ebc90d2694ceda946d1e0
tesseradecades <nwe8978@rit.edu>
2019-05-17 20:29:24 -0400

    added speed and gitignore

---
+++ test/DnD/Abilities/test_Speed.py
@@ -0,0 +1,17 @@
+import unittest
+from main.DnD.Abilities.Speed import Speed
+from main.DnD.Character import Character
+from main.DnD.Races.Race import Race
+
+class TestConcreteMethod(unittest.TestCase):
+    def test_ability_score_increases(self):
+        #Arrange
+        speed = Speed(30)
+        character = Character(Race())
+        #Act
+        speed.concreteEffect(character)
+        #Assert
+        self.assertIs(character.speed,30)
+
+if __name__ == '__main__':
+    unittest.main()
